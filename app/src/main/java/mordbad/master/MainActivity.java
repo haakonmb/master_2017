99d5a5aab8234d33d312d2a8d551c2e38a5e7e93
Haakon Borch <haakonborch@gmail.com>
2016-04-21 15:11:10 +0200

    Commit-test for differentiating new project from this.

--- app/src/main/java/mordbad/master/MainActivity.java
+++ app/src/main/java/mordbad/master/MainActivity.java
@@ -2,15 +2,19 @@ package mordbad.master;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    String TAG = "mainactivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(TAG, "test");
     }
 
     @Override
