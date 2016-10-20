4ba5c6bd27c51e495f3a8121d2f61f487832e142
Haakon Borch <haakonborch@gmail.com>
2016-10-20 21:38:47 +0200

    ADDED: watchmaker library. Added directly since jitpack didnt want to include it with github-link. Check up on that later. CHANGED: startet up inclusion of watchmaker in Reasoner-class

--- app/src/main/java/mordbad/master/dss/Reasoner.java
+++ app/src/main/java/mordbad/master/dss/Reasoner.java
@@ -1,6 +1,19 @@
 package mordbad.master.dss;
 
+
+
+import com.google.android.gms.location.places.Place;
+
+import org.uncommons.watchmaker.framework.EvolutionEngine;
+import org.uncommons.watchmaker.framework.GenerationalEvolutionEngine;
+import org.uncommons.watchmaker.framework.factories.BitStringFactory;
+import org.uncommons.watchmaker.framework.factories.ObjectArrayPermutationFactory;
+import org.uncommons.watchmaker.framework.operators.ObjectArrayCrossover;
+import org.uncommons.watchmaker.framework.selection.RouletteWheelSelection;
+import org.uncommons.math.random.MersenneTwisterRNG;
+//import mordbad.master.R;
 import mordbad.master.data.Gatherer;
+
 
 /**
  * Created by haakon on 12/05/16.
@@ -9,10 +22,50 @@ public class Reasoner {
 
     private Gatherer gatherer = new Gatherer();
     private Wish wish = null;
+    private PlaceEvaluator pe;
 
 
     public Reasoner(){};
 
+
+
+
+    public Reasoner(String[] candidates){
+        /*
+        What watchmaker needs:
+    A Candidate Factory
+    An Evolutionary Operator
+    A Fitness Evaluator
+    A Selection Strategy
+    A Random Number Generator
+
+     */
+
+        //Candidate factory
+        ObjectArrayPermutationFactory<String> factory = new ObjectArrayPermutationFactory<>(candidates);
+
+        //Evolutionary Operator
+        ObjectArrayCrossover<String> operator = new ObjectArrayCrossover<>();
+
+        //Fitness Evaluator
+        pe = new PlaceEvaluator();
+
+
+        //Selection Strategy
+        RouletteWheelSelection rouletteWheelSelection = new RouletteWheelSelection();
+
+        //Random number generator
+
+
+
+        //private EvolutionEngine<BitStringFactory> test = new GenerationalEvolutionEngine<BitStringFactory>();
+
+
+
+
+
+
+    };
 
 
 
