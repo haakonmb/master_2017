55033f20dc6ed45ea2a7e640f6e769605768694b
Haakon Borch <haakonborch@gmail.com>
2017-04-10 18:22:30 +0200

    ADDED: new class for new stuff

---
+++ app/src/main/java/mordbad/master/dss/Probabilitator.java
@@ -0,0 +1,14 @@
+package mordbad.master.dss;
+
+import java.util.Map;
+
+/**
+ * Created by haakon on 10/04/17.
+ */
+
+public class Probabilitator {
+    int[] prior_probabilities;
+    Map<Integer, Double> evidence_probabilities;
+
+
+}
