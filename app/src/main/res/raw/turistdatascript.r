bf76f851276ffe702df0ea4a293b87ec3df773b0
Haakon Borch <haakonborch@gmail.com>
2017-04-10 11:22:04 +0200

    CHANGED: made it so it generates apriori probabilities.

--- app/src/main/res/raw/turistdatascript.r
+++ app/src/main/res/raw/turistdatascript.r
@@ -14,18 +14,32 @@ for(num in c(1:6)){
   names.values[num] <- length(names.values[[num]])
 }
 
-probability <- vector("list",276)
+probability.independent <- vector("list",11)
+probability.dependent <- vector("list",275)
 counter <- 1
 
 for(name in c(1:length(names))){
   for(values in c(1:names.values[[name]])){
     for(column in c(7:17)){
-      counter <- counter +1
+      
        tmp <-              table(data[which(data[names[name]] == values ),column])
        tmp <- tmp[1] / sum(tmp)
-      probability[[counter]] <- c(name,values,column,tmp)
+      probability.dependent[[counter]] <- c(name,values,column,tmp)
+      counter <- counter +1
      print(c(names[name],c(values,'/',names.values[[name]]),column,tmp))
     }
   }      
 }
 
+counter <- 1
+for(column in c(7:17)){
+  tmp <-              table(data[,column])
+  tmp <- tmp[1] / sum(tmp)
+  probability.independent[counter] <- tmp
+  counter <- counter +1
+}
+
+
+
+write.csv(probability.independent,"probability.apriori.csv")
+write.csv(probability.dependent,"probability.postpriori.csv")
