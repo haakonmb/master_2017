89f282de0cb082319792b0c640ba5c9c03770f10
Haakon Borch <haakonborch@gmail.com>
2017-04-19 17:44:58 +0200

    CHANGED: sends probabilitator to Tourfragmnt. Ugly ugly.

--- app/src/main/java/mordbad/master/MainActivity.java
+++ app/src/main/java/mordbad/master/MainActivity.java
@@ -383,6 +383,9 @@ public class MainActivity extends AppCompatActivity implements PreferenceFragmen
     public void startActivityEvaluation(int[] dataFromQuestions) {
         adjustedProbabilities = new Probabilitator(priors_from_data, lookup_probability, dataFromQuestions);
         data_adjusted_probabilities = Observable.just(adjustedProbabilities.map_activities_to_probability_for_yes);
+//                .subscribe(TourFragment.getObserver());
+        TourFragment.setProbabilitator(adjustedProbabilities);
+
     }
 
     @Override
