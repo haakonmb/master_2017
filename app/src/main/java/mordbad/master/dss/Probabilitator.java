f0abd7a3f212d711efa185c6ae8f326da032c6a8
Haakon Borch <haakonborch@gmail.com>
2017-04-19 11:17:23 +0200

    CHANGED: probabilitator changed somehow. This is how it goes when you wait to commit stuff. Nothing important probably.

--- app/src/main/java/mordbad/master/dss/Probabilitator.java
+++ app/src/main/java/mordbad/master/dss/Probabilitator.java
@@ -7,8 +7,22 @@ import java.util.Map;
  */
 
 public class Probabilitator {
-    int[] prior_probabilities;
+    double[] prior_probabilities;
     Map<Integer, Double> evidence_probabilities;
+    int[] data;
+    double[] probabilities = new double[11];
+
+    public Probabilitator(double[] priors, Map<Integer,Double> evidence, int[] data){
+        this.prior_probabilities = priors;
+        this.evidence_probabilities = evidence;
+        this.data= data;
+        calculateProbabilities();
+
+    }
+
+    private void calculateProbabilities() {
+        
+    }
 
 
 }
