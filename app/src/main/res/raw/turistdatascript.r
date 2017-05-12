efcdc27d005822a8ad40380ef2921da3f7699a0b
Haakon Borch <haakonborch@gmail.com>
2017-05-12 19:11:57 +0200

    CHANGED: added some stuff for making pretty graphs from Viktor

--- app/src/main/res/raw/turistdatascript.r
+++ app/src/main/res/raw/turistdatascript.r
@@ -57,7 +57,7 @@ labels <- vector("list",6)
 labels[[1]] <- c("Visiting friends", "Other vacation and freetime-activities")
 labels[[2]] <- c("Norway","Europe","America","Asia","Other")
 labels[[3]] <- c("Male","Female","Other")
-labels[[4]] <- c("1-18","19-30","31-40","41-50","51-60","60+")
+labels[[4]] <- c("1-29","30-45","46-60","60+")
 labels[[5]] <- c("Lower Secondary","Higher Secondary","Bachelor","Master","Researcher","Dont know")
 labels[[6]] <- c("Employed","Self-employed","Pensioner","Student","Other","Dont know")
 
@@ -76,9 +76,42 @@ for(i in c(1:6)){
   percent[[counter]] <- round(values[[counter]]/sum(values[[counter]])*100)
   labels[[counter]] <- paste(labels[[counter]],percent[[counter]])
   labels[[counter]] <- paste(labels[[counter]],"%",sep="")
-  pie(values[[counter]],labels=labels[[counter]],col=rainbow(length(labels[[counter]])),main = questions[[counter]])
+ # pie(values[[counter]],labels=labels[[counter]],col=rainbow(length(labels[[counter]])),main = questions[[counter]])
+ plot(percent[[counter]],ylim=range(1:100),type="h")
   counter <- counter +1
 }
+
+
+#viktor sin plot
+a <-c(percent[[1]][[1]],percent[[1]][[2]])
+a <- c(percent[[2]][[1]],percent[[2]][[2]],percent[[2]][[3]],percent[[2]][[4]],percent[[2]][[5]])
+a <- c(percent[[3]][[1]],percent[[3]][[2]])
+a <- c(percent[[4]][[1]],percent[[4]][[2]],percent[[4]][[3]],percent[[4]][[4]])
+a <- c(percent[[5]][[1]],percent[[5]][[2]],percent[[5]][[3]],percent[[5]][[4]],percent[[5]][[5]],percent[[5]][[6]])
+a <- c(percent[[6]][[1]],percent[[6]][[2]],percent[[6]][[3]],percent[[6]][[4]],percent[[6]][[5]],percent[[6]][[6]])
+
+
+b <- c("Visiting friends", "Other vacation and freetime-activities")
+b <- c("Norway","Europe","America","Asia","Other")
+b <- c("Male","Female")
+b <- c("1-29","30-45","46-60","60+")
+b <- c("Lower Secondary","Higher Secondary","Bachelor","Master","Researcher","Dont know")
+b <- as.character(b)
+b <- factor(b, levels=unique(b))
+
+
+b <- c("Employed","Self-employed","Pensioner","Student","Other","Dont know")
+df <- data.frame(a,b)
+df
+
+ggplot(data = df,aes(b, weight = a)) +  
+  geom_bar(aes(fill = b)) +  
+  theme_bw() +  
+  scale_fill_brewer(palette = "Blues") +   
+  xlab("Features") +  
+  ylab("% of .....") +  
+  coord_cartesian(ylim = c(0,100))
+
 
 #Old evidence-probabilities generator
 # for(name in c(1:length(names))){
